c436b3a7585501997307fc1f5286b36d306a8a90
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-12-26 23:26:35 +0530

    Using selector in different ways

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers',      // selecting based on the custom tag
+  // selector: '.app-servers',  // selecting based on the class named as 'app-servers'
+  // selector: '[app-servers]', // selecting based on the attribute named as 'app-selector'
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
