28d672e55a28abc15a82a01c9877b8e84a484c27
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-12-26 23:15:03 +0530

    Using templates & styles and using inline method for it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: '<app-servers></app-servers>',
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: red;
+    }
+  `]
 })
 export class AppComponent {
 
