9b29cd07757786f1b220eb0230c22b78ba186eb3
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-12-27 15:21:08 +0530

    Using ngStyle and ngClass directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 
 export class ServerComponent {
