4ff53b103105b28c4f17636a73acb279cf579de6
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-12-29 11:40:31 +0530

    Component communication using custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ export class AppComponent {
     {type: 'server', name: 'Testserver', content: 'Testing techniques'}
   ];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: { blueprintName: string, blueprintContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
+
 }
