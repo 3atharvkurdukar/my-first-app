40448d7bcb0cf9686a0929f8b21aa9342d3aee54
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-03 10:02:54 +0530

    Blank start for branching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [
-    {type: 'server', name: 'Testserver', content: 'Testing techniques'}
-  ];
-
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onBlueprintAdded(blueprintData: { blueprintName: string, blueprintContent: string }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.blueprintName,
-      content: blueprintData.blueprintContent
-    });
-  }
 
 }
