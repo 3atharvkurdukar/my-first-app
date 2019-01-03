3b6147d0bd767535bdf7981975fa0c3f61865cb8
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-03 10:50:10 +0530

    Providing relative paths using Router and Route classes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,3 +1,4 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
 
@@ -7,12 +8,19 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  private servers: {id: number, name: string, status: string}[] = [];
+  servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    this.router.navigate(['./'], {relativeTo: this.route});
   }
 
 }
