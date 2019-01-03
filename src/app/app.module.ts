4e50cdce1974b4e6d7bf3ab662712e247e0d944d
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-03 15:43:26 +0530

    Apply dynamic links and data passing through links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,18 +14,10 @@ import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
-  {
-    path: '',           // http://localhost:4200/
-    component: HomeComponent
-  },
-  {
-    path: 'users',           // http://localhost:4200/users
-    component: UsersComponent
-  },
-  {
-    path: 'servers',           // http://localhost:4200/servers
-    component: ServersComponent
-  }
+  { path: '', component: HomeComponent},
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
+  { path: 'servers', component: ServersComponent }
 ];
 
 @NgModule({
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  private paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
