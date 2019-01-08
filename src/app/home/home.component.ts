1e65568b6be28a9742752a9749fa70cbee49e065
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-09 00:01:21 +0530

    Creating a custom observer and subscribing it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs-compat';
+import { Observable, Observer } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +11,22 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
-      (number: number) => console.log(number)
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => console.log(number)
+    // );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => observer.next('First Package'), 2000);
+      setTimeout(() => observer.next('Second Package'), 4000);
+      // setTimeout(() => observer.error('This does not work'), 5000);
+      setTimeout(() => observer.complete(), 5000);
+      setTimeout(() => observer.next('Third Package'), 6000);   // It will never get executed
+    });
+    myObservable.subscribe(
+      (data: string) => console.log(data),
+      (error: string) => console.log(error),
+      () => console.log('Completed!')
     );
   }
 
