c109aea9c85fcfb0a2ff1c9e9fde48d3666a3002
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-11 21:58:09 +0530

    Created a reactive form with custom validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,40 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  projectForm: FormGroup;
 
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      // 'name': new FormControl(null, [Validators.required, this.forbiddenName]),
+      'name': new FormControl(null, Validators.required, this.forbiddenNameAsync),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl(null)
+    });
   }
 
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
+
+  forbiddenName(control: FormControl): {[s: string]: boolean} {
+    if (control.value === 'Test') {
+      return {'nameForbidden': true};
+    } else {
+      return null;
+    }
+  }
+
+  forbiddenNameAsync(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === 'Test') {
+            resolve({'nameForbidden': true});
+          } else {
+            resolve(null);
+          }
+        }, 1500);
+      }
+    );
+  }
 }
