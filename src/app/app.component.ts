b4e7381bc48ef741e1aa1f50e44ef480d7122458
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-23 19:48:15 +0530

    Initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  servers = [
+    {
+      name: 'Testserver',
+      capacity: 10,
+      id: this.generateId()
+    },
+    {
+      name: 'Liveserver',
+      capacity: 100,
+      id: this.generateId()
+    }
+  ];
+  onAddServer(name: string) {
+    this.servers.push({
+      name: name,
+      capacity: 50,
+      id: this.generateId()
+    });
+  }
+  private generateId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
